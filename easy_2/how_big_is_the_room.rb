5408c9dddd147d624bd08aa8539d2983b1fbd4a0
aaroncallagher <me@aaroncallagher.com>
2016-09-21 17:23:15 +0000

    Exercise Solution Refactoring

--- easy_2/how_big_is_the_room.rb
+++ easy_2/how_big_is_the_room.rb
@@ -1,3 +1,5 @@
+SQ_METERS_TO_FEET = 10.7639
+
 puts "Enter the length of the room in meters:"
 length = gets.chomp.to_f
 
@@ -5,6 +7,6 @@ puts "Enter the width of the room in meters:"
 width = gets.chomp.to_f
 
 sq_meters = length * width
-sq_feet = sq_meters * 10.7639
+sq_feet = (sq_meters * SQ_METERS_TO_FEET).round(2)
 
-puts "The area of the room is #{sq_meters} square meters (#{sq_feet.round(2)} square feet)"
+puts "The area of the room is #{sq_meters} square meters (#{sq_feet} square feet)"
