769977844c4e3f6b819ab1fa2ebc92efb053e627
aaroncallagher <me@aaroncallagher.com>
2016-09-22 18:37:30 +0000

    Added Exercise Solution

--- easy_2/when_will_i_retire.rb
+++ easy_2/when_will_i_retire.rb
@@ -1,5 +1,5 @@
 require 'date'
-
+CURRENT_YEAR = Time.now.year
 
 puts "What is your age?"
 age = gets.chomp.to_i
@@ -8,5 +8,5 @@ retire_age = gets.chomp.to_i
 
 years_to_retirement = retire_age - age
 
-puts "It's #{Date.new(2016).year}. You will retire in #{2016 + years_to_retirement}"
+puts "It's #{CURRENT_YEAR}. You will retire in #{CURRENT_YEAR + years_to_retirement}"
 puts "You only have #{years_to_retirement} years to go!"
