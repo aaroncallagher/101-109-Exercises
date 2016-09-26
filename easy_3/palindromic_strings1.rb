aa653505cdb75e5aa18e442c8e2b8a08856e140d
aaroncallagher <me@aaroncallagher.com>
2016-09-26 22:17:26 +0000

    Added Exercise Solution

---
+++ easy_3/palindromic_strings1.rb
@@ -0,0 +1,11 @@
+def palindrome?(str)
+  forward = str
+  backwards = str.reverse
+  
+  forward == backwards
+end
+
+puts palindrome?('madam') == true
+puts palindrome?('Madam') == false          # (case matters)
+puts palindrome?("madam i'm adam") == false # (all characters matter)
+puts palindrome?('356653') == true
