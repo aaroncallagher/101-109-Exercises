5e840bf21bf4c290ad8b5e3a075a0464f947ce9a
aaroncallagher <me@aaroncallagher.com>
2016-10-10 21:57:21 +0000

    Added Exercise Solution

---
+++ easy_8/fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(begin_num, end_num)
+  (begin_num..end_num).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
+end
+
+p fizzbuzz(1, 15)
